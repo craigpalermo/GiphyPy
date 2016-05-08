c4d6526872e1d8104065feb6e34aba0d6202e17d
Craig Palermo <craig.palermo@opmbx.org>
2016-05-07 21:25:30 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# GiphyPy
+
+A command-line tool to get a gif in a jif.
+
+GiphyPy will search for an image using the [Giphy API](https://github.com/Giphy/GiphyAPI#search-endpoint)
+and return the first image that matches your query.
+
+## Usage
+```
+$ python giphy.py "dogs in space"
+http://media2.giphy.com/media/QHffgxXFqJJoA/200.gif
+```
+
+## TODO
+- Command-line args for gif rating and number of results
---
+++ giphy.py
@@ -0,0 +1,71 @@
+import argparse
+import json
+import requests
+
+# Configuration
+giphy = {
+    "api_key": "dc6zaTOxFJmzC",
+    "search_url": "http://api.giphy.com/v1/gifs/search",
+    "rating": "r",
+    "image_size_prefs": ["fixed_height", "fixed_height_small", "downsized", "original"]
+}
+
+# Parse CI args
+parser = argparse.ArgumentParser(description="Search for a random GIF!")
+parser.add_argument('query', type=str, help='Search for something, in GIF form.')
+
+def giphy_search(query):
+    '''
+    Query the Giphy search API to retrieve gifs that match the given query string
+    :param query:
+    :return results:
+    '''
+    payload = {"q": query, "api_key": giphy["api_key"], "limit": 3}
+    r = requests.get(giphy["search_url"], params=payload)
+    results = json.loads(r.text) if r.status_code == requests.codes.ok else None
+    return results
+
+
+def select_gif(response_data):
+    '''
+    Returns the URL of the first item in the given response data. If response contains no results,
+    then return None.
+    :param response_data:
+    :return result:
+    '''
+    data = response_data["data"]
+    result = None
+
+    if len(data) > 0:
+        images = response_data["data"][0]["images"]
+
+        if images:
+            # Get first available image size preference
+            image_size = None
+
+            for key in giphy["image_size_prefs"]:
+                if key in images:
+                    image_size = key
+                    break
+
+            result = images[image_size]["url"]
+
+    return result
+
+
+def main():
+    # Get command line arguments
+    args = parser.parse_args()
+
+    # Make request to Giphy API to search for a gif
+    response = giphy_search(args.query)
+
+    if response:
+        gif = select_gif(response)
+        print(gif)
+    else:
+        print("Oops, we weren't able to GIF you anything.")
+
+
+if __name__ == "__main__":
+    main()
